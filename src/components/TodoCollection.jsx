7df389b1c62738cf4b5b8d5170b57411fc73414f
Noemin Lin <noeminlin@gmail.com>
2022-11-25 16:53:16 +0800

    完成新增及編輯功能

--- src/components/TodoCollection.jsx
+++ src/components/TodoCollection.jsx
@@ -1,14 +1,18 @@
 import TodoItem from './TodoItem';
 
-const TodoCollection = () => {
+const TodoCollection = ({ todos, onToggleDown, onSave, onDelete, onChangeMode }) => {
   return (
     <div>
       TodoCollection
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todos.map(todo =>
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onSave={({ id, title }) => { onSave?.({ id, title }) }}
+          onToggleDown={(id) => { onToggleDown?.(id) }}
+          onChangeMode={({ id, isEdit }) => onChangeMode({ id, isEdit })}
+        />
+      )}
     </div>
   );
 };
