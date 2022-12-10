fbbb0f1474980c6cc5c26694f6225be3e31db4b6
Noemin Lin <noeminlin@gmail.com>
2022-12-10 20:27:00 +0800

    change todo api url

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,11 +1,24 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3004';
+const baseUrl = 'Link：https://todo-list.alphacamp.io/api';
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+  return config;
+}, (error) => {
+  console.error('[]', error)
+});
 
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
-    return res.data;
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
+    return res.data.data;
   } catch (err) {
     console.error('[Get Todos failed]:', err);
   }
@@ -14,12 +27,12 @@ export const getTodos = async () => {
 export const createTodo = async (payload) => {
   try {
     const { title, isDone } = payload;
-    const res = await axios.post(`${baseUrl}/todos`, {
+    const res = await axiosInstance.post(`${baseUrl}/todos`, {
       title,
       isDone,
     });
 
-    return res.data;
+    return res.data.data;
   } catch (err) {
     console.error('[Greate Todos failed]:', err);
   }
@@ -28,12 +41,12 @@ export const createTodo = async (payload) => {
 export const updateTodo = async (payload) => {
   const { id, title, isDone } = payload;
   try {
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
       title,
       isDone,
     });
 
-    return res.data;
+    return res.data.data;
   } catch (err) {
     console.log('[Update Todos failed]:', err);
   }
@@ -41,8 +54,8 @@ export const updateTodo = async (payload) => {
 
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`);
-    return res.data
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
+    return res.data.data
   } catch (err) {
     console.log('[Delete Todos failed]:', err);
   }
